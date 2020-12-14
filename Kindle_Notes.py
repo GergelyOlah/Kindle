ab0e5c75dbdded29ae511d07098c1f4e1554576f
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-14 21:11:21 +0000

    File opening added

--- Kindle_Notes.py
+++ Kindle_Notes.py
@@ -1,7 +1,18 @@
 note_file = open("My_Notes.txt")
 
+paragraph = []
+extracted_notes = []
 
 for line in note_file:
-    print(type(line))
-        
+    if not line.startswith("==="):
+        paragraph.append(line)
+    else:
+        for i in range(len(paragraph)-1,-1,-1):
+            if not paragraph[i].startswith("- Your"):
+                extracted_notes.append(paragraph[i])
+            else:
+                break
+
+print(extracted_notes)
+
 note_file.close()

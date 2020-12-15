fb35c235b8632bb9a89a4cd58ce3fa137e65dff6
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-15 19:05:16 +0000

    Extraction added

--- Kindle_Notes.py
+++ Kindle_Notes.py
@@ -1,20 +1,19 @@
-note_file = open("My_Notes.txt")
+with open("My_Notes.txt") as note_file:
 
-paragraph = []
-extracted_notes = []
+    paragraph = []
+    extracted_notes = []
+    book_titles = [] 
 
-for line in note_file:
-    if not line.startswith("==="):
-        paragraph.append(line)
-    else:
-        for i in range(len(paragraph)-1,-1,-1):
-            if not paragraph[i].startswith("- Your"):
-                extracted_notes.append(paragraph[i])
-            else:
-                break
+    for line in note_file:
+        if not line.startswith("==="):
+            paragraph.append(line)
+        else:
+            for i in range(len(paragraph)-1,-1,-1):
+                if not paragraph[i].startswith("- Your"):
+                    extracted_notes.append(paragraph[i])
+                else:
+                    break
 
-with open("Notes_Book_3.txt", "w") as extracted_file:
-    for line in extracted_notes:
-        extracted_file.write(line)
-
-note_file.close()
+    with open("Notes_Book_3.txt", "w") as extracted_file:
+        for line in extracted_notes:
+            extracted_file.write(line)
